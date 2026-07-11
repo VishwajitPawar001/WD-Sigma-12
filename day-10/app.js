f38f6de7252a1f5082c977d7be47c4e6bb4c5b75
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-07-11 12:34:06 +0530

    "feat: Learnt about objects and Math functions in javascript and implemented in code"

---
+++ day-10/app.js
@@ -0,0 +1,16 @@
+const dice = Math.floor(Math.random()*6) +1;
+
+let car = {
+    name: "Mahindra",
+    model: "xuv",
+    color: "black"
+};
+
+console.log(`The name of the car in object is ${car.name}`);
+
+let Person = {
+    name: "Vishwajit",
+    age: 20,
+    city: "Nanded"
+};
+
