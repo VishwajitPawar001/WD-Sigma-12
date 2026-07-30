0aff8b99f8b97f35858e6db65760055cc7f308cb
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-07-30 21:14:07 +0530

    Learnt about EJS templates

--- day-18/index.js
+++ day-18/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = 5000;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res)=>{
-    res.status(200).send("<h1> App is running </h1>");
+    res.render("home.ejs");
 });
+
+app.get("/hello", (req, res)=>{
+    res.send("hello");
+});
+
+app.get("/roledice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("roledice.ejs", {diceVal});
+})
 
 app.listen(PORT, () =>{
     console.log(`Port running at ${PORT}`);
