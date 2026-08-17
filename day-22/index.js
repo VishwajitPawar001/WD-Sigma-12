0b59205f655bbfffe2ec86eb686709d08c0299a6
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-08-17 21:39:00 +0530

    Day - 22

---
+++ day-22/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+main()
+  .then(() => {
+    console.log('Connection Successful');
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+const user2 = new User({
+  name: 'Eve',
+  email: 'Eve@yahoo.com',
+  age: 20,
+});
+
+user2
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
